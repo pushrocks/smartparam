b3364a31426c850a47ca4ceb909834eb709e5b7a
Phil Kunz <github@phil.today>
2015-09-21 19:17:04 +0000

    update

--- ts/index.ts
+++ ts/index.ts
@@ -1,14 +1,17 @@
 /// <reference path="typings/tsd.d.ts" />
-var path, through;
+var bl = require("beautylog");
 
-through = require("through2");
-path = require("path");
-
-module.exports = (jadeTemplate,mojo = undefined) => {
-
-    return through.obj((file, enc, cb) => {
-        
-        //run callback function to signal end of plugin process.
-        return cb(null, file);
-    });
+module.exports = (parentObject,childParam:string,action:string = 'smartAdd') => {
+    if (action = 'smartAdd') {
+        if(parentObject.hasOwnProperty(childParam)){
+            mojo.log("param " + childParam + " exists!");
+        } else {
+            mojo.log('file.data does not exist, so we make it!');
+            file.data = {}
+        }
+        //make mojo settings available for jade through mojo.something
+        if (mojoActive) {
+            file.data.mojo = mojo.settings;
+        }
+    }
 };
