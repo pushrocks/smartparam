07d95cdf3aee7ad08835147a24f38cef20b4e173
Phil Kunz <gitlab@philkunz.com>
2020-01-31 14:40:21 +0000

    feat(now deeply adds params): update

--- test/test.ts
+++ test/test.ts
@@ -10,8 +10,17 @@ tap.test('childObject should not yet be in parentObject', async () => {
   parentObject.childObject = childObject;
 });
 
-tap.test('childObject should now BE in parentObject', async () => {
+tap.test('childObject should now be in parentObject', async () => {
   expect(smartparam.exists(parentObject, 'childObject')).to.be.true;
+});
+
+tap.test('should be able to deepAdd an childParam', async () => {
+  const parentObject = {
+    hello: 'there'
+  };
+  const parentObject2 = smartparam.smartAdd(parentObject, 'wow.za', 'yes');
+  console.log(parentObject2)
+  expect(smartparam.exists(parentObject2.wow, 'za')).to.be.true;
 });
 
 tap.test('should call properties for minimatched properties', async () => {
--- ts/index.ts
+++ ts/index.ts
@@ -1,20 +1,72 @@
 import plugins = require('./smartparam.plugins');
 
 /**
- * adds an obejct to the parent object if it doesn't exists
+ * adds an object to the parent object if it doesn't exists
  * @param parentObject
  * @param childParam
  * @param logBool
  * @returns {boolean}
  */
-export let smartAdd = function(parentObject, childParam: string): boolean {
-  if (parentObject.hasOwnProperty(childParam)) {
-    return false;
+export const smartAdd = (parentObject, childParam: string, valueArg = {}, optionsArg?: {
+  interpretDotsAsLevel: boolean;
+}): typeof parentObject & any => {
+
+  optionsArg = {
+    interpretDotsAsLevel: true,
+    ...optionsArg
+  }
+
+  let varNamesArray: string[];
+  let referencePointer = parentObject;
+  if (optionsArg.interpretDotsAsLevel) {
+    varNamesArray = childParam.split('.');
   } else {
-    parentObject[childParam] = {};
-    return true;
+    varNamesArray = [childParam];
+  }
+
+  for (let i = 0; i !== varNamesArray.length; i++) {
+    const varName = varNamesArray[i];
+
+    // is there a next variable ?
+    const varNameNext: string = (() => {
+      if (varNamesArray[i + 1]) {
+        return varNamesArray[i + 1];
+      }
+      return null;
+    })();
+
+    // build the tree in parentObject
+    if (!referencePointer[varName] && !varNameNext) {
+      referencePointer[varName] = valueArg;
+      referencePointer = null;
+    } else if (!referencePointer[varName] && varNameNext) {
+      referencePointer[varName] = {};
+      referencePointer = referencePointer[varName];
+    } else if (referencePointer[varName] && varNameNext) {
+      referencePointer = referencePointer[varName];
+    } else {
+      throw new Error('Something is strange!');
+    }
+  }
+  return parentObject
+};
+
+/**
+ * gets an object from the parent object using dots as levels by default
+ * @param parentObject 
+ * @param childParam 
+ * @param optionsArg 
+ */
+export const smartGet = (parentObject, childParam: string, optionsArg?: {
+  interpretDotsAsLevel: boolean;
+}) => {
+  optionsArg = {
+    interpretDotsAsLevel: true,
+    ...optionsArg
   }
 };
+
+
 
 /**
  * checks if an object has a parameter with a given key name, returns true if yes.
